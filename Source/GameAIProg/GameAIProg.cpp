bcd2c8ddddb91b190b1945426f02a8cd5880bddf
Casper van Laer <caspervlaer@outlook.com>
2026-06-30 16:01:53 +0200

    redoing research

--- Source/GameAIProg/GameAIProg.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "GameAIProg.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GameAIProg, "GameAIProg" );
-
-DEFINE_LOG_CATEGORY(LogGameAIProg)
- 
